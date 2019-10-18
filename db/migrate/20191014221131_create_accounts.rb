6dd2827cb93dbbfb2136beae28706c746c33d2f9
Samir <samirphaddad@gmail.com>
2019-10-17 22:24:22 -0300

    Breaking changing in migration

--- db/migrate/20191014221131_create_accounts.rb
+++ db/migrate/20191014221131_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
-      t.decimal :balance, :precision => 2, :scale => 2, :default => 0
+      t.decimal :balance, :precision => 2, :scale => 32, :default => 0
       t.string :number
       t.integer :status, :limit => 2
       t.references :user
